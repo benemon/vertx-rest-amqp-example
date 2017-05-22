103f1d5309446f07f2047c6f9d8f272d69b267e6
Benjamin Holmes <benjamin.holmes@gmail.com>
2017-05-22 22:20:04 +0100

    Updated constants to match OCP defaults

--- src/main/java/io/openshift/common/CommonConstants.java
+++ src/main/java/io/openshift/common/CommonConstants.java
@@ -2,12 +2,12 @@ package io.openshift.common;
 
 public interface CommonConstants {
 	
-	public static final String AMQP_BROKER_USER_ENV = "AMQP_BROKER_USER";
-	public static final String AMQP_BROKER_PASSWORD_ENV = "AMQP_BROKER_PASSWORD";
-	public static final String AMQP_BROKER_HOST_ENV = "AMQP_BROKER_HOST";
-	public static final String AMQP_BROKER_PORT_ENV = "AMQP_BROKER_PORT";
+	public static final String AMQP_BROKER_USER_ENV = "BROKER_AMQ_AMQP_SERVICE_USER";
+	public static final String AMQP_BROKER_PASSWORD_ENV = "BROKER_AMQ_AMQP_SERVICE_PASSWORD";
+	public static final String AMQP_BROKER_HOST_ENV = "BROKER_AMQ_AMQP_SERVICE_HOST";
+	public static final String AMQP_BROKER_PORT_ENV = "BROKER_AMQ_AMQP_SERVICE_PORT";
 	
-	public static final String AMQP_BROKER_ADDRESS_ENV = "AMQP_BROKER_ADDRESS";
+	public static final String AMQP_BROKER_ADDRESS_ENV = "BROKER_AMQ_AMQP_SERVICE_ADDRESS";
 	
 	
 
